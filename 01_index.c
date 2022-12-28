fab81872b5e32bafa3c515cd386c5ddbb8699d56
mariselvam07 <rlmselvam7@gmail.com>
2022-12-28 06:48:55 +0530

    added new file

---
+++ 01_index.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //for printf and scanf
+
+
+int main() // compiler check first main function
+{
+    printf("Hellow World!\n");
+    return 0;
+}
---
+++ 02_Data_Types.c
@@ -0,0 +1,43 @@
+/**************************************************************
+                DATA TYPES IN C PROGRAMMING
+
+    WHAT IS DATA?
+        DESCRIPTION             VALUES
+        1. CAPITAL LETTERS      A-Z
+        2. SMALL LETTERS        a-z
+        3. NUMBERS              0-9
+        4. SYMBOLS              * , / $ %.....
+
+    DATA TYPES :-
+        
+1. Build in/Primitive Data Type
+    a. Integral Data Type
+            1. int
+            2. char
+    b. Boolean Data Type
+    c. Floating Point Data Type
+            1. float
+            2. Double
+    d. Void Data Type
+2. Derived Data Type
+    a. Array Data Type
+    b. Pointers Data Type
+3. User Defined Data Type 
+    a. struct
+    b. enum
+    c. union      
+
+Integer Datatype
+    An integer is a Whole number that can be positive, negative, or zero.
+            int - %d(format specifier)
+
+Character Datatype
+    Character is a data type that holds one character(letter, number, etc.) of data. It must be enclosed with single quotes. e.g.'A'.'4',or'#'
+            char - %c(fprmat specifier)
+
+Floating Point Datatype
+            float - %f(format specifier)
+
+
+
+*/
